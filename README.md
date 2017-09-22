65a2f90460937d712bdbc7cfb524474f93c394ec
Vladimi Kurguzov <kurguzov74@gmail.com>
2017-09-22 20:21:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Webpack для SPA сайта :<br />
+npm i :Импортировать необходимые модули.Зависимости указаны в файле package.json
+
+dependencies":<br />
+    "assets-webpack-plugin": "^3.5.1",<br />
+    "autoprefixer-loader": "^3.1.0",<br />
+    "babel": "^6.0.15",<br />
+    "babel-core": "^6.0.14",<br />
+    "babel-loader": "^6.0.0",<br />
+    "bundle-loader": "^0.5.5",<br />
+    "babel-preset-es2015": "^6.0.14",<br />
+    "copy-webpack-plugin": "^4.0.1",<br />
+    "css-loader": "^0.21.0",<br />
+    "extract-text-webpack-plugin": "^0.8.2",<br />
+    "file-loader": "^0.8.4",<br />
+    "html-webpack-plugin": "^2.30.1",<br />
+    "jade": "^1.11.0",<br />
+    "jade-loader": "^0.7.1",<br />
+    "node-static": "^0.7.7",<br />
+    "rimraf": "^2.6.2",<br />
+    "style-loader": "^0.13.0",<br />
+    "stylus-loader": "^1.4.0",<br />
+    "url-loader": "^0.5.6",<br />
+    "webpack": "^1.12.2",<br />
+    "webpack-dev-server": "^1.12.1"
+
+# !!!!!!!!!!ЗАПУСКАТЬ ТОЛЬКО ЧЕРЕЗ npm run скрипт!!!!!!!!!!
+
+# npm run devser       -- http://localhost:8080/webpack-dev-server
+# npm run testser      -- http://127.0.0.1:3000/
+# npm run prod         -- http://127.0.0.1:3000/ + АРХИВАЦИЯ frontend В old_frontend!!!
+
+scripts в package.json:<br />
+1. "error": "webpack --display-error-details",
+2. "info": "webpack --display-modules",
+3. "infov": "webpack --display-modules –v",
+4. "time": "webpack --profile --display-modules",
+5. "timev": "webpack --profile --display-modules --display-reasons",
+6. "devser": "set NODE_ENV=devser&set NODE_ARCH=0&webpack-dev-server --inline --hot",
+7. "testser": "set NODE_ENV=testser&set NODE_ARCH=0&webpack&node server.js",
+8. "prod": "set NODE_ENV=prod&set NODE_ARCH=1&webpack&node server.js"
+
+Плагины:
+1. NoErrorsPlugin – не добавляет в сборку файлы с ошибками
+2. rimraf - удаляет старые js файлы из папки
+3. assetsPlugin - СОЗДАЕТ ФАЙЛ assets.json С ИНФОРМАЦИЕЙ О СБОРКЕ (hesh)
+4. HtmlWebpackPlugin - создает index.html файл с подключенным внутри и обновленным app.[hash].js
+5. CopyWebpackPlugin - копирует файлы
+6. ExtractTextPlugin - создает CSS файл
+
+!!! ПРИ ВНЕСЕНИЕ ИЗМЕНЕНИЙ В JS ФАЙЛЫ - ПЕРЕЗАГРУЗИТЬ = npm run devser
+
+ЗАГОЛОВОК HTML СТРАНИЦЫ - const titleHTML = 'ЗАГОЛОВОК';
---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+'use strict';
+const NODE_ENV = process.env.NODE_ENV || 'devser';
+const NODE_ARCH = process.env.NODE_ARCH || 0;
+
+const webpack = require('webpack');
+//assetsPlugin - СОЗДАЕТ ФАЙЛ assets.json С ИНФОРМАЦИЕЙ О СБОРКЕ (hesh)
+const assetsPlugin = require('assets-webpack-plugin');
+const rimraf = require('rimraf'); //УДАЛЯЕТ СТАРЫЕ JS ФАЙЛЫ ИЗ ПАПКИ
+//HtmlWebpackPlugin - СОЗДАЕТ ФАЙЛ /index.html С ОБНОВЛЕННЫМ ПОДКЛЮЧЕННЫМ ФАЙЛОМ app.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');//КОПИРУЕТ ФАЙЛЫ 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");//СОЗДАЕТ CSS ФАЙЛ
+
+const titleHTML = 'ЗАГОЛОВОК';
+let t = Date.parse(new Date()); //milliseconds
+
+if (NODE_ENV == 'devser') {
+  module.exports = {
+    context: __dirname + '/frontend',
+
+    entry: {  // --inline --hot
+      main: './main'
+    },
+
+    output: {
+      path:       __dirname + '/public',
+      publicPath: '/',
+      filename:   '[name].js'
+    },
+
+    module: {
+      loaders: [{
+        test:    /\.js$/,
+        include: __dirname + '/frontend',
+        loader:  "babel?presets[]=es2015"
+      }, {
+        test:   /\.jade$/,
+        loader: "jade"
+      }, {
+        test:   /\.styl$/,
+        loader: ExtractTextPlugin.extract('style', 'css!stylus?resolve url')
+      }, {
+        test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+        loader: 'file?name=[path][name].[ext]?[hash]'
+      }]
+    },
+
+    plugins: [
+      new ExtractTextPlugin('[name].css', {allChunks: true, disable: false})
+    ],
+
+    devServer: {
+      contentBase: __dirname + '/backend',
+      hot: true
+    }
+  };
+}else{
+  module.exports = {
+    context: __dirname + '/frontend',
+
+    entry: {
+      app: './main'
+    },
+
+    output: {
+      path:     __dirname + '/public',
+      publicPath: '/',  //   /js/app.js
+      filename: "[name][hash].js"
+    },
+
+    module: {
+      loaders: [{
+        test:    /\.js$/,
+        include: __dirname + '/frontend',
+        loader:  "babel?presets[]=es2015"
+      }, {
+        test:   /\.jade$/,
+        loader: "jade"
+      }, {
+        test:   /\.styl$/,
+        loader: ExtractTextPlugin.extract('style', 'css!stylus?resolve url')
+      }, {
+        test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+        loader: 'file?name=[path][name][hash].[ext]'
+      }]
+    },
+
+    plugins: [
+      {
+        apply: (compiler) => {
+          rimraf.sync(compiler.options.output.path);
+        }
+      },
+      new ExtractTextPlugin('[name][hash].css', {allChunks: true, disable: false}),
+      new webpack.NoErrorsPlugin(),
+      new assetsPlugin ({
+        filename: 'assets.json',
+        path: __dirname + '/public'
+      }),
+      new HtmlWebpackPlugin({title: titleHTML}),
+    ],
+  };
+};
+
+if(NODE_ARCH == 1){
+  console.log('!!!!!!WEBPACK - АРХИВАЦИЯ!!!!! ОТМЕНА - npm run devser ИЛИ npm run testser')
+  module.exports.plugins.push(
+    new CopyWebpackPlugin([// Copy directory contents to {output}/to/directory/
+        { from: __dirname + '/frontend', to: __dirname + '/old_frontend/'+ t +'/' }
+    ])
+  )
+}else{
+  console.log('!!!!!!WEBPACK - БЕЗ АРХИВАЦИИ!!!!!')
+};
